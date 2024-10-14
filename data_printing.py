9b993fffddd6d6a16fc405fb8eb2fec63c221cab
SMOG01 <PavlovPavel94@yandex.ru>
2024-10-14 15:49:10 +0300

    Средняя цена закрытия акции за период

--- data_printing.py
+++ data_printing.py
@@ -1,5 +1,6 @@
 from data_processing import get_average_close_price
 
+
 def display_average_close_price(stock_data,
                                 period):
     average_price = round(get_average_close_price(stock_data), 2)

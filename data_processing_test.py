a5a927496a4d504cffe47fc28f7f45dd9ebd8e2b
SMOG01 <PavlovPavel94@yandex.ru>
2024-10-17 21:50:26 +0300

    Тестирование переделано на pytest

--- data_processing_test.py
+++ data_processing_test.py
@@ -1,12 +1,9 @@
-import unittest as ut
 import pandas as pd
 
 from data_processing import get_average_close_price
 
 
-class DataProcessingTest(ut.TestCase):
-    def test_get_average_close_price(self):
-        test_data = pd.DataFrame({'Close': [400, 500, 600, 700, 800]})
-        average_close_price = get_average_close_price(test_data)
+def test_get_average_close_price():
+    test_data = pd.DataFrame({'Close': [400, 500, 600, 700, 800]})
 
-        self.assertAlmostEqual(average_close_price, 600)
+    assert get_average_close_price(test_data) == 600
